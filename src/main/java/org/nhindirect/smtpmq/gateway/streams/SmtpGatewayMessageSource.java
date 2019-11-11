15bacf19907f3233281150bf500b61e5038411ce
gm2552 <gm2552@c02rl1r4g8wl>
2019-11-11 06:31:09 -0600

    Adding some logging and updating to spring boot 2.1.10.

--- src/main/java/org/nhindirect/smtpmq/gateway/streams/SmtpGatewayMessageSource.java
+++ src/main/java/org/nhindirect/smtpmq/gateway/streams/SmtpGatewayMessageSource.java
@@ -1,24 +1,47 @@
 package org.nhindirect.smtpmq.gateway.streams;
 
+import java.util.List;
+
+import javax.mail.internet.InternetAddress;
+
 import org.nhindirect.common.mail.SMTPMailMessage;
 import org.nhindirect.common.mail.streams.SMTPMailMessageConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
 
+
 @EnableBinding(SmtpGatewayMessageOutput.class)
 public class SmtpGatewayMessageSource
 {	
+	private static final Logger LOGGER = LoggerFactory.getLogger(SmtpGatewayMessageSource.class);	
+	
 	@Autowired
 	@Qualifier(SmtpGatewayMessageOutput.SMTP_GATEWAY_MESSAGE_OUTPUT)
 	private MessageChannel smtpGatewayChannel;
 	
 	@Output(SmtpGatewayMessageOutput.SMTP_GATEWAY_MESSAGE_OUTPUT)
-	public <T> void forwardSMTPMessage(SMTPMailMessage msg) 
+	public <T> void forwardSMTPMessage(SMTPMailMessage msg) throws Exception
 	{
+		LOGGER.info("Handing off incoming message to smtp gateway for from {} to {} with message id {}", msg.getMailFrom().toString(), 
+				toRecipsPrettingString(msg.getRecipientAddresses()), msg.getMimeMessage().getMessageID());
+		
 		this.smtpGatewayChannel.send(SMTPMailMessageConverter.toStreamMessage(msg));
 	}
 
+	protected String toRecipsPrettingString(List<InternetAddress> recips)
+	{
+		final String[] addrs = new String[recips.size()];
+		
+		int idx = 0;
+		for (InternetAddress addr : recips)
+			addrs[idx++] = addr.toString();
+		
+		return String.join(",", addrs);
+	}
+	
 }
